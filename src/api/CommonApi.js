eb6dde2f8b6560c641603657d34799a7c6c9f4d3
Vu Huy Hieu <hieubeobb@gmail.com>
2020-04-06 19:53:51 +0700

    create api

--- src/api/CommonApi.js
+++ src/api/CommonApi.js
@@ -1,1 +1,32 @@
 import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
+
+const demoToken = 'ff0f2d93-006d-3aaa-94ec-05ee1a1ff2af';
+
+const _getHeader = async () => {
+  const token = await AsyncStorage.getItem('access_token');
+  if (token) {
+    return {
+      token: token
+    };
+  }
+  return {
+    token: demoToken
+  };
+};
+
+export const callApi = async (method, url, input) => {
+  const headers = await _getHeader();
+  const configs = {
+    headers:headers,
+    method: method,
+    url: url,
+    data: input
+  };
+  await axios(configs).then((response)=>{
+
+  }).catch((error)=>{
+    console.error('call api error: ',error);
+  })
+};
