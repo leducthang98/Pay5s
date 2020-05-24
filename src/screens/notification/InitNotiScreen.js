01caa39eed5d8484da7659e706a97878fa5f1a52
Vu Huy Hieu <hieubeobb@gmail.com>
2020-05-24 16:31:53 +0700

    change code in WebView

--- src/screens/notification/InitNotiScreen.js
+++ src/screens/notification/InitNotiScreen.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    Image,
-    Dimensions
+  View,
+  Text,
+  Image,
+  Dimensions,
 } from 'react-native';
-import Header from '../../components/common/Header'
-import { ScrollView } from 'react-native-gesture-handler';
-import { scale, scaleVertical, scaleModerate } from '../../constant/Scale';
-import { PRIMARY_COLOR, FACEBOOK } from '../../constant/Colors'
+import Header from '../../components/common/Header';
+import {ScrollView} from 'react-native-gesture-handler';
+import {scale, scaleVertical, scaleModerate} from '../../constant/Scale';
+import {PRIMARY_COLOR, FACEBOOK} from '../../constant/Colors';
+
 const containerW = Dimensions.get('window').width;
 const containerH = Dimensions.get('window').height;
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
+import {statusBarHeight} from '../../constant/Layout';
+import LoadingDialog from '../../components/common/LoadingDialog';
+
 class InitNotiScreen extends React.Component {
-    render() {
-
-        let data = this.props.route.params;
-        console.log(data.dataNotification)
-        return (
-            <View style={{ flex: 1, backgroundColor: 'white' }}>
-                <Header navigation={this.props.navigation} back={true} title={'Chi tiết'} />
-                <ScrollView>
-
-                    <WebView style={{ width: containerW, height: scale(1700) }} source={{ uri: data.dataNotification.content }} />
-
-                </ScrollView>
-            </View>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
     }
+  }
+  render() {
+    let data = this.props.route.params;
+    console.log(data.dataNotification);
+    return (
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Header navigation={this.props.navigation} back={true} title={'Chi tiết'}/>
+        <WebView
+          source={{uri: data.dataNotification.content}}
+          style={{marginBottom: statusBarHeight}}
+          onLoad={()=>this.setState({isLoading:false})}
+        />
+        {this.state.isLoading && <LoadingDialog />}
+      </View>
+    );
+  }
 
 }
+
 export default InitNotiScreen;
