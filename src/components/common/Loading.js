837ef7ce4e3e2d17f550042cfccdbc3cec16a3fe
@vuhuyhieu <hieuvh@nganluong.vn>
2020-06-18 21:09:00 +0700

    fix bug

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,43 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { View, ActivityIndicator, StyleSheet, Modal, Text,Dimensions } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, ActivityIndicator, StyleSheet, Modal, Text, Dimensions} from 'react-native';
 import * as COLOR from '../../constant/Colors';
+
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default function Loading(props) {
-    const loading = props.isLoading
-    return (
-        <View style={styles.container}>
-            <View style={styles.loadingContainer}>
-                <ActivityIndicator size={'large'} color={COLOR.PURPLE_FONTCOLOR} />
-                <Text style={styles.textLoading}>Loading</Text>
-            </View>
+  const loading = props.isLoading;
+  return (
+    <Modal
+      visible={true}
+      transparent={true}
+    >
+      <View style={styles.container}>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLOR.PURPLE_FONTCOLOR}/>
+          <Text style={styles.textLoading}>Loading</Text>
         </View>
-    );
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: width,
-        height: height,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#00000099',
-        position: 'absolute',
-        elevation: 8,
-    },
-    loadingContainer: {
-        width: width * 0.8,
-        height: height / 10,
-        backgroundColor: '#FFF',
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderRadius: 8,
-        paddingLeft: 10,
-        opacity: 1,
-    },
-    textLoading: {
-        fontSize: 14,
-        marginLeft: 10,
-    },
+  container: {
+    width: width,
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00000099',
+    position: 'absolute',
+    elevation: 8,
+  },
+  loadingContainer: {
+    width: width * 0.8,
+    height: height / 10,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    paddingLeft: 10,
+    opacity: 1,
+  },
+  textLoading: {
+    fontSize: 14,
+    marginLeft: 10,
+  },
 });
