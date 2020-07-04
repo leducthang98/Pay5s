0214c85e0f3e29b5d87d137fb3d87b5288c0b06f
Lê Đức Thắng <leducthang98@gmail.com>
2020-07-05 01:38:49 +0700

    Complete the rest of KPlus

--- src/screens/k_plus/KPlus.js
+++ src/screens/k_plus/KPlus.js
@@ -13,37 +13,37 @@ import {
 } from 'react-native';
 import ItemRechargeList from '../../components/recharge/ItemRechargeList';
 import ChooseServiceAndPhone from '../../components/recharge/ChooseServiceAndPhone';
-import {getString} from '../../res/values/String';
+import { getString } from '../../res/values/String';
 import * as COLOR from '../../constant/Colors';
-import {texts} from '../../constant/CommonStyles';
-import {scale, scaleModerate, scaleVertical} from '../../constant/Scale';
+import { texts } from '../../constant/CommonStyles';
+import { scale, scaleModerate, scaleVertical } from '../../constant/Scale';
 import * as Layout from '../../constant/Layout';
-import {getRechargePhoneServiceAPI} from '../../fetchAPIs/getRechargePhoneServiceAPI';
+import { getRechargePhoneServiceAPI } from '../../fetchAPIs/getRechargePhoneServiceAPI';
 import LoadingDialog from '../../components/common/LoadingDialog';
 import MessageDialog from '../../components/common/MessageDialog';
 import ChooseNetwork from '../../components/recharge/ChooseNetwork';
-import {isPhoneNumber} from '../../constant/Validate';
-import {setPhoneNumberForRecharge} from '../../actions/ActionBillScreen';
-import {connect} from 'react-redux';
-import {md5Signature} from '../../constant/Secure';
-import {CONFIRM_BILL_CREATE, LOGIN} from '../../navigators/RouteName';
+import { isPhoneNumber } from '../../constant/Validate';
+import { setPhoneNumberForRecharge } from '../../actions/ActionBillScreen';
+import { connect } from 'react-redux';
+import { md5Signature } from '../../constant/Secure';
+import { CONFIRM_BILL_CREATE, LOGIN } from '../../navigators/RouteName';
 import Toast from 'react-native-simple-toast';
 import AsyncStorage from '@react-native-community/async-storage';
-import {CommonActions} from '@react-navigation/native';
+import { CommonActions } from '@react-navigation/native';
 import LinearButton from '../../components/common/LinearButton';
 import LinearGradient from 'react-native-linear-gradient';
 import ChooseEPINService from '../../components/recharge/ChooseEPINService';
-import {getKPlusService} from '../../fetchAPIs/KPlusApi';
+import { getKPlusService } from '../../fetchAPIs/KPlusApi';
 import Header from '../../components/common/Header';
 import ItemKPlus from '../../components/recharge/ItemKPlus';
 import ItemAdditionPrice from '../../components/recharge/ItemAdditionPrice';
-import {formatMoney} from '../../constant/CommonFormat';
+import { formatMoney } from '../../constant/CommonFormat';
 
-const {width, height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 const additionCard = [
-  {value: 0, isSelected: true},
-  {value: 1, isSelected: false},
-  {value: 2, isSelected: false},
+  { value: 0, isSelected: true },
+  { value: 1, isSelected: false },
+  { value: 2, isSelected: false },
   // {value: 4, isSelected: false},
   // {value: 5, isSelected: false},
   // {value: 6, isSelected: false},
@@ -74,11 +74,11 @@ class KPlus extends React.Component {
 
   async componentDidMount() {
     await this._fetchData();
-    const {data} = this.state;
+    const { data } = this.state;
     if (data) {
-      const {months} = data;
+      const { months } = data;
       const parsedMonths = this._addSelectedProps(months);
-      this.setState({months: parsedMonths});
+      this.setState({ months: parsedMonths });
     }
     this._calculateDiscountAmount();
     this._processAdditionPrice();
@@ -88,7 +88,7 @@ class KPlus extends React.Component {
 
   UNSAFE_componentWillReceiveProps(props) {
     if (props.billReducer !== this.props.billReducer) {
-      this.setState({phoneNumber: props.billReducer?.phoneNumberForRecharge});
+      this.setState({ phoneNumber: props.billReducer?.phoneNumberForRecharge });
     }
   }
 
@@ -102,22 +102,22 @@ class KPlus extends React.Component {
     this.props.route?.navigation?.dispatch(
       CommonActions?.reset({
         index: 1,
-        routes: [{name: LOGIN}],
+        routes: [{ name: LOGIN }],
       }),
     );
   };
 
   _fetchData = async () => {
-    this.setState({isLoading: true});
+    this.setState({ isLoading: true });
     const response = await getKPlusService();
-    this.setState({isLoading: false});
+    this.setState({ isLoading: false });
     const data = response?.data;
-    this.setState({data});
-    this.setState({months: data?.months});
+    this.setState({ data });
+    this.setState({ months: data?.months });
   };
 
   _selectItemKPlus = (selectedItem, selectedIndex) => {
-    const {months} = this.state;
+    const { months } = this.state;
     selectedItem.isSelected = true;
     months?.map((item, index) => {
       if (item.isSelected && index !== selectedIndex) {
@@ -125,12 +125,12 @@ class KPlus extends React.Component {
       }
     });
     months[selectedIndex] = selectedItem;
-    this.setState({months});
+    this.setState({ months });
     this._calculateTotalAmount();
   };
 
   _selectItemAdditionCard = (selectedItem, selectedIndex) => {
-    const {additionCard} = this.state;
+    const { additionCard } = this.state;
     selectedItem.isSelected = true;
     additionCard?.map((item, index) => {
       if (item.isSelected && index !== selectedIndex) {
@@ -138,13 +138,13 @@ class KPlus extends React.Component {
       }
     });
     additionCard[selectedIndex] = selectedItem;
-    this.setState({additionCard});
+    this.setState({ additionCard });
     this._calculateTotalAmount();
   };
 
   _processAdditionPrice = () => {
-    const {addition_cnt} = this.state.data;
-    let {additionCard} = this.state;
+    const { addition_cnt } = this.state.data;
+    let { additionCard } = this.state;
 
   };
 
@@ -152,10 +152,10 @@ class KPlus extends React.Component {
     let newData = [];
     data?.map((item, index) => {
       if (index === 0) {
-        const newItem = Object.assign(item, {isSelected: true});
+        const newItem = Object.assign(item, { isSelected: true });
         newData.push(newItem);
       } else {
-        const newItem = Object.assign(item, {isSelected: false});
+        const newItem = Object.assign(item, { isSelected: false });
         newData.push(newItem);
       }
     });
@@ -163,24 +163,24 @@ class KPlus extends React.Component {
   };
 
   _calculateDiscountAmount = () => {
-    const {months} = this.state;
+    const { months } = this.state;
     let newMonths = [];
     months?.map((item, index) => {
       const discount = item?.discount;
       const discountAmount = item.price * discount / 100 || 0;
-      const newItem = Object.assign(item, {discountAmount, discount});
+      const newItem = Object.assign(item, { discountAmount, discount });
       newMonths.push(newItem);
     });
-    this.setState({months: newMonths});
+    this.setState({ months: newMonths });
   };
 
   _checkActive = () => {
-    const {contractId, contractError, totalAmount} = this.state;
+    const { contractId, contractError, totalAmount } = this.state;
     return (!contractError && contractId !== '' && totalAmount !== 0);
   };
 
   _onTypingPhoneNumber = contractId => {
-    this.setState({contractId});
+    this.setState({ contractId });
     if (contractId === '') {
       this.setState({
         contractError: true,
@@ -200,32 +200,42 @@ class KPlus extends React.Component {
 
   _onPressDeposit = async () => {
     this._onKeyboardDismiss();
-    const {contractId, error} = this.state;
+    const { contractId, error } = this.state;
     if (this._checkActive()) {
-      // viết logic thanh toán ở đây
+      const { months, data, additionCard } = this.state;
+      const itemKPlusSelected = await months?.filter(item => item.isSelected === true);
+      const itemAdditionPriceSelected = await additionCard?.filter(item => item.isSelected === true);
+      let dataKPlus = {
+        contract: contractId,
+        month: itemKPlusSelected[0]?.price,
+        addition: itemAdditionPriceSelected[0]?.value
+      }
+      console.log(dataKPlus)
     }
   };
 
   _calculateTotalAmount = async () => {
-    const {months, data, additionCard} = this.state;
+    const { months, data, additionCard } = this.state;
     const itemKPlusSelected = await months?.filter(item => item.isSelected === true);
     const itemAdditionPriceSelected = await additionCard?.filter(item => item.isSelected === true);
     const additionPrice = data.addition_price;
     const totalAmount = await Number(itemKPlusSelected[0]?.price) - Number(itemKPlusSelected[0]?.discountAmount)
       + Number(itemAdditionPriceSelected[0]?.value) * Number(additionPrice);
-    if (!isNaN(totalAmount)){
-      this.setState({totalAmount});
+    if (!isNaN(totalAmount)) {
+      this.setState({ totalAmount });
     }
   };
-
+  _checkValidPhoneNumber(phoneNumber) {
+    console.log("Anh Hieu lam thieu cho nay :D")
+  }
   render() {
-    const {isLoading, error, data, contractIndex, months, additionCard} = this.state;
-    const {contractId, contractError, contractErrorContent} = this.state;
+    const { isLoading, error, data, contractIndex, months, additionCard } = this.state;
+    const { contractId, contractError, contractErrorContent } = this.state;
     const isEPIN = this.props.route?.service === 'EPIN';
     if (isLoading) {
       return (
-        <View style={[styles.container, {alignItems: 'center', justifyContent: 'center'}]}>
-          <LoadingDialog/>
+        <View style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+          <LoadingDialog />
         </View>
       );
     }
@@ -235,8 +245,8 @@ class KPlus extends React.Component {
         {/*  contentContainerStyle={{flex: 1, alignItems: 'center', backgroundColor: COLOR.BACKGROUND_COLOR}}*/}
         {/*  behavior={'padding'}>*/}
         <View
-          style={{alignItems: 'center'}}>
-          <Header back={true} navigation={this.props.navigation} title={'Gia hạn K+'}/>
+          style={{ alignItems: 'center' }}>
+          <Header back={true} navigation={this.props.navigation} title={'Gia hạn K+'} />
           <ChooseServiceAndPhone
             kPlusService={true}
             error={contractError}
@@ -248,49 +258,49 @@ class KPlus extends React.Component {
             note={data?.note}
             service={this.props.route?.service}
           />
-          <View style={{width, paddingHorizontal: scaleModerate(15)}}>
-            <Text style={[texts.l_h4, {fontWeight: 'bold'}]}>{'Số tháng'}</Text>
+          <View style={{ width, paddingHorizontal: scaleModerate(15) }}>
+            <Text style={[texts.l_h4, { fontWeight: 'bold' }]}>{'Số tháng'}</Text>
           </View>
           <FlatList
-            style={{marginTop: scaleVertical(5)}}
+            style={{ marginTop: scaleVertical(5) }}
             numColumns={3}
             data={months}
             nestedScrollEnabled={true}
             extraData={this.state}
             keyExtractor={(item, index) => index}
-            renderItem={({item, index}) => <ItemKPlus
+            renderItem={({ item, index }) => <ItemKPlus
               data={item}
-              onPress={() => this._selectItemKPlus(item, index)}/>}
+              onPress={() => this._selectItemKPlus(item, index)} />}
           />
 
-          <View style={{width, paddingHorizontal: scaleModerate(15)}}>
-            <Text style={[texts.l_h4, {fontWeight: 'bold'}]}>{'Số thẻ phụ'}</Text>
+          <View style={{ width, paddingHorizontal: scaleModerate(15) }}>
+            <Text style={[texts.l_h4, { fontWeight: 'bold' }]}>{'Số thẻ phụ'}</Text>
           </View>
           <FlatList
-            style={{marginTop: scaleVertical(5)}}
+            style={{ marginTop: scaleVertical(5) }}
             numColumns={3}
             data={additionCard}
             nestedScrollEnabled={true}
             extraData={this.state}
             keyExtractor={(item, index) => index}
-            renderItem={({item, index}) => <ItemAdditionPrice
+            renderItem={({ item, index }) => <ItemAdditionPrice
               data={item}
-              onPress={() => this._selectItemAdditionCard(item, index)}/>}
+              onPress={() => this._selectItemAdditionCard(item, index)} />}
           />
-          <View style={{width, paddingHorizontal: scaleModerate(15)}}>
-            <Text style={[texts.l_h4, {fontWeight: 'bold'}]}>
+          <View style={{ width, paddingHorizontal: scaleModerate(15) }}>
+            <Text style={[texts.l_h4, { fontWeight: 'bold' }]}>
               {'Tổng: ' + formatMoney(this.state.totalAmount) + ' đ'}
             </Text>
           </View>
 
-          <View style={{width, paddingHorizontal: scaleModerate(15), marginTop: scaleVertical(20)}}>
+          <View style={{ width, paddingHorizontal: scaleModerate(15), marginTop: scaleVertical(20) }}>
             <Text style={[texts.l_sm]}>{this.state.data?.description}</Text>
           </View>
-          {isLoading && <LoadingDialog/>}
+          {isLoading && <LoadingDialog />}
           {
             error !== null && <MessageDialog
               message={error.message}
-              close={() => this.setState({error: null})}
+              close={() => this.setState({ error: null })}
             />
           }
         </View>
@@ -298,13 +308,19 @@ class KPlus extends React.Component {
         <View style={[styles.buttonArea]}>
           <TouchableOpacity
             // style={styles.button}
-            style={{width: '100%', height: scaleVertical(50)}}
-            onPress={this._onPressDeposit}>
+            disabled={this.state.contractId ? false : true}
+            style={{ width: '100%', height: scaleVertical(50) }}
+            onPress={() => this._onPressDeposit()}>
             <LinearGradient
-              start={{x: 0, y: 0.75}} end={{x: 1, y: 0.25}}
+              start={{ x: 0, y: 0.75 }} end={{ x: 1, y: 0.25 }}
               colors={['#ff547c', '#c944f7']}
               style={styles.button}>
-              <Text style={texts.white_bold}>{'THANH TOÁN'}</Text>
+              <Text style={{
+                fontSize: scaleModerate(14),
+                fontWeight: 'bold',
+                color: (this.state.contractId) ? COLOR.WHITE : "silver",
+                textAlign: 'center',
+              }}>{'THANH TOÁN'}</Text>
             </LinearGradient>
           </TouchableOpacity>
         </View>
